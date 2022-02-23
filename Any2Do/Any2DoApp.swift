f6bb5449bda8706402c9391a462e893d77423c56
M1nt666 <87382339+M1nt666@users.noreply.github.com>
2022-02-23 23:02:38 +0800

    Initial Commit

---
+++ Any2Do/Any2DoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Any2DoApp.swift
+//  Any2Do
+//
+//  Created by Mint on 2022/2/23.
+//
+
+import SwiftUI
+
+@main
+struct Any2DoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
